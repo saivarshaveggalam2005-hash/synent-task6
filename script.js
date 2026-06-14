7f18f678afedd936b8b9e876db0c8e8860ae2c26
saivarshaveggalam2005-hash <saivarshaveggalam2005@gmail.com>
2026-06-14 19:12:56 +0530

    initial setup

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getQuote() {
+
+    const quote = document.getElementById("quote");
+    const author = document.getElementById("author");
+
+    quote.innerText = "Loading...";
+    author.innerText = "";
+
+    try {
+
+        const response = await fetch(
+            "https://dummyjson.com/quotes/random"
+        );
+
+        const data = await response.json();
+
+        quote.innerText = `"${data.quote}"`;
+        author.innerText = `- ${data.author}`;
+
+    } catch (error) {
+
+        quote.innerText = "Failed to load quote.";
+        console.error(error);
+    }
+}
